6aa46be10bc16a6667ad0fd01dff6097b577ccc8
Chris Dukakis <chrisdukakis@gmail.com>
2020-07-27 02:37:01 +0300

    implement dissemination for node.js

---
+++ dissemination/browser.js
@@ -0,0 +1,2 @@
+export { dissemination } from './src/dissemination-browser.js'
+export { delayQueue } from './src/delay-queue.js'
--- dissemination/index.js
+++ dissemination/index.js
@@ -1,2 +1,2 @@
-export { dissemination } from './src/dissemination.js'
+export { dissemination } from './src/dissemination-node.js'
 export { delayQueue } from './src/delay-queue.js'
